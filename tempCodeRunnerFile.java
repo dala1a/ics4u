4d085bffdd7b1591a36afb6fb2198cd41f0aa4a2
dala1a <0jeonyunseo1107@gmail.com>
2025-02-09 18:26:10 -0500

    Delete tempCodeRunnerFile.java

--- tempCodeRunnerFile.java
+++
@@ -1,9 +0,0 @@
- System.out.println(
-                "Menu \n Enter a number! \n" 
-                + "\n 1. Read in Data" 
-                + "\n 2. Sort list by student lastname" 
-                + "\n 3. Print out a table"
-                + "\n 4. Write file with new sorted data file"
-                + "\n 5. Search for a Student Number"
-                + "\n 6. Exit"
-            );
