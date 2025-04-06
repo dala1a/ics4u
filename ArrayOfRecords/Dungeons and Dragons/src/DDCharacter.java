d19a4dc46e1fb35ded39923979770946f8723626
dala1a <0jeonyunseo1107@gmail.com>
2025-04-06 19:53:13 -0400

    Update DDCharacter.java

--- ArrayOfRecords/Dungeons and Dragons/src/DDCharacter.java
+++ ArrayOfRecords/Dungeons and Dragons/src/DDCharacter.java
@@ -1,58 +1,113 @@
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.util.Random;
+import java.util.Scanner;
+
 public class DDCharacter {
-    private String Name;
-    private String Race;
-    private String Class;
-    private int Level;
-    private int hitPoints;
-    private int Strength;
-    private int Constitution;
-    private int Intelligence;
-    private int Wisdom;
-    private int Dexterity;
-    private int Charisma;
-   
-    public void setName (String newName) {
-        this.Name = newName;
+    private String characterName; // 40 Bytes (20 Characters)
+    private String race; // 20 Bytes (10 Characters)
+    private String playerClass; // 20 Bytes (10 Characters)
+    private int level; // 4 Bytes
+    private int hitPoints; // 4 Bytes
+    private int strength; // 4 Bytes
+    private int constitution; // 4 Bytes
+    private int intelligence; // 4 Bytes
+    private int wisdom; // 4 Bytes
+    private int dexterity; // 4 Bytes
+    private int charisma; // 4 Bytes
+    private final long recLen = 112; // Length of each record
+
+    public DDCharacter (String characterName, String race, String playerClass, int level, int hitPOints, int strength, int constitution, int intelligence, int wisdom, int dexterity, int charisma) {
+        this.characterName = characterName;
+        this.race = race;
+        this.playerClass = playerClass;
+        this.level = level;
+        this.hitPoints = hitPOints;
+        this.strength = strength;
+        this.constitution = constitution;
+        this.intelligence = intelligence;
+        this.wisdom = wisdom;
+        this.dexterity = dexterity;
+        this.charisma = charisma;
     }
 
-    public void setRace (String newRace) {
-        this.Race = newRace;
+    public DDCharacter () {
+        characterName = "";
+        race = "";
+        playerClass = "";
+        level = 0;
+        hitPoints = 0;
+        strength = 0;
+        constitution = 0;
+        intelligence = 0;
+        wisdom = 0;
+        dexterity = 0;
+        charisma = 0;
     }
 
-    public void setClass (String newClass) {
-        this.Class = newClass;
+    public void setName(String characterName) {
+        this.characterName = characterName;
     }
 
-    public void setLevel (int newLevel) {
-        this.Level = newLevel;
+    public void setRace(String race) {
+        this.race = race;
     }
 
-    public void setHitPoints (int newHitPoints) {
+    public void setClass(String playerClass) {
+        this.playerClass = playerClass;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public void setHitPoints(int newHitPoints) {
         this.hitPoints = newHitPoints;
     }
 
-    public void setStrength (int newStrength) {
-        this.Strength = newStrength;
-    }
-    public void setConstitution (int newConstitution) {
-        this.Constitution = newConstitution;
+    public void setStrength(int strength) {
+        this.strength = strength;
     }
 
-    public void setIntelligence (int newIntelligence) {
-        this.Intelligence = newIntelligence;
+    public void setConstitution(int constitution) {
+        this.constitution = constitution;
     }
 
-    public void setWisdom (int newWisdom) {
-        this.Wisdom = newWisdom;
+    public void setIntelligence(int intelligence) {
+        this.intelligence = intelligence;
     }
 
-    public void setDexterity (int newDexterity) {
-        this.Dexterity = newDexterity;
+    public void setWisdom(int wisdom) {
+        this.wisdom = wisdom;
     }
 
-    public void setCharisma (int newCharisma) {
-        this.Charisma = newCharisma;
+    public void setDexterity(int dexterity) {
+        this.dexterity = dexterity;
     }
+
+    public void setCharisma(int charisma) {
+        this.charisma = charisma;
+    }
+    
+    public int getDiceRoll(int max) {
+        Random rand = new Random(); // Create a Random object
+        int result = rand.nextInt(max) + 1; // Generate a random number between 1 and max (inclusive)
+        return result;
+    }
+
+    public int randomStatGen() {
+
+        return 0;
+    }
+
+    public void levelUp() {
+
+    }
+
+    public void ChangeClass() {
+
+    }
+
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
     }
